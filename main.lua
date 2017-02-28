7f668a6cef119f5c68fca80161754a1e3e4ecdc9
Chris Bode <chris@eonz.net>
2017-02-28 15:25:59 -0500

    added hit detection and knockback physics to combat prototype

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-
 local Vector = require 'eonz.vector'
 local ECS = require 'eonz.ecs'
 
@@ -15,17 +14,29 @@ local keys = {
 
 --
 
-local PlayerDrawable
+local function Enemy(params) 
+  params = (type(params) == 'table' and params) or {}
+  
+  return {
+    mass = 1 or params.mass,
+    health = 3 or params.health
+  }
+end
+
+local spawnEnemy
 
 local function PlayerController()
-  local PLAYER_ACCEL = 800
-  local PLAYER_DAMP = 0.05
+  local PLAYER_ACCEL = 1000
+  local PLAYER_DAMP = 0.0001
   
-  local SWORD_SWING_TIME = 0.2
-  local SWORD_ANGLE = math.pi * 2/3
+  local SWORD_SWING_TIME = 0.05
+  local SWORD_HOLD_TIME = 0.5
+  local SWORD_ANGLE = math.pi * 0.8
   
   local SWORD_RADIUS = 48
   local SWORD_INNER_RADIUS = 24
+  
+  local KNOCK_BACK_IMPULSE = 1000
   
   local nextSwingDirection = 1
   
@@ -37,29 +48,75 @@ local function PlayerController()
     nextSwingDirection = nextSwingDirection * -1
     
     return {
-      dir     = dir, -- normal vector that bisects the swing arc
-      swing   = swingDir, -- the sign of the swing motion
-      time    = SWORD_SWING_TIME, -- the remaining duration of the swing in seconds
-      maxTime = SWORD_SWING_TIME, -- the total duration of the swing
-      theta   = SWORD_ANGLE, -- the total angle covered by the swing
-      radius  = { outer=SWORD_RADIUS, inner=SWORD_INNER_RADIUS }, -- the size of the swing in pixels
-      color   = color or {255, 0, 0}
+      dir       = dir, -- normal vector that bisects the swing arc
+      swing     = swingDir, -- the sign of the swing motion
+      
+      time      = 0,
+      swingTime = SWORD_SWING_TIME, -- the duration of the swing in seconds
+      holdTime  = SWORD_HOLD_TIME, -- the duration of the swing in seconds
+      
+      theta     = SWORD_ANGLE, -- the total angle covered by the swing
+      radius    = { outer=SWORD_RADIUS, inner=SWORD_INNER_RADIUS }, -- the size of the swing in pixels
+      color     = color or {255, 0, 0}
+      
     }
     
   end
   
-  local function attack(e, dt)
-    local dir = (Vector.new(love.mouse.getPosition()) - e.pos):normalized()
-    e.sword = Sword(dir)
+  local function normalize(angle)
+    while angle > math.pi * 2 do
+      angle = angle - math.pi * 2
+    end
+    
+    while angle < 0 do
+      angle = angle + math.pi * 2
+    end
+    
+    return angle
   end
   
-  return function(e, dt)
+  local function attack(e, dt, ecs)
+    local dir = (Vector.new(love.mouse.getPosition()) - e.pos):normalized()
+    
+    e.sword = Sword(dir)
+    
+    local hitDist = SWORD_RADIUS 
+    
+    for id, entity in ecs:each() do
+      if entity.enemy then
+        
+        local toEntity = entity.pos - e.pos
+        local dist = toEntity:length() - (entity.radius * 2) 
+                
+        if dist <= hitDist then
+          local angleTo = toEntity:getAngle()
+          local lowerAngle = e.sword.dir:getAngle() - e.sword.theta / 2
+          local relAngle = normalize(angleTo - lowerAngle)
+          
+          if relAngle < e.sword.theta then
+            entity.enemy.health = entity.enemy.health - 1
+            
+            if entity.enemy.health <= 0 then
+              ecs:destroy(entity)
+              
+              spawnEnemy(ecs)
+            else
+              local knockBack = (e.sword.dir + toEntity:normalized()) / 2 * KNOCK_BACK_IMPULSE / entity.enemy.mass
+              entity.vel = (entity.vel or Vector.zero()) + knockBack
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  return function(e, dt, ecs)
     
     if e.sword then
       e.vel = Vector.zero()
       
-      e.sword.time = e.sword.time - dt
-      if e.sword.time <= 0 then
+      e.sword.time = e.sword.time + dt
+      if e.sword.time >= e.sword.holdTime then
         e.sword = nil
       end
     end
@@ -67,7 +124,7 @@ local function PlayerController()
     -- Sword Control
     
     if not e.sword and love.mouse.isDown(1) then
-      attack(e, dt)
+      attack(e, dt, ecs)
     end
     
     -- Movement Control
@@ -101,7 +158,7 @@ local function PlayerController()
 end
 
 local function ChaseController()
-  local CHASE_ACCEL = 200
+  local CHASE_ACCEL = 100
   local CHASE_DAMP = 0.1
   local CHASE_IMPULSE = 500
   
@@ -134,11 +191,11 @@ end
 local function CircleDrawable(radius, rgba)
   return function(e)
     love.graphics.setColor(rgba or e.color or {255, 255, 255})
-    love.graphics.circle('fill', 0, 0, radius, 32)
+    love.graphics.circle('fill', 0, 0, radius or e.radius, 32)
   end
 end
 
-PlayerDrawable = function(avatar)
+local function PlayerDrawable(avatar)
   
   local SWORD_BLADE_SIZE = 0.1
   
@@ -147,14 +204,14 @@ PlayerDrawable = function(avatar)
       local sword = e.sword
       
       local angle = sword.dir:getAngle()
-      local phase = 1 - sword.time / sword.maxTime
+      local phase = math.min(1, sword.time / sword.holdTime)
       
       -- These control the rate at which the swing and fade
       -- animations occur relative to the total duration
       -- of the sword animation.
       
-      local swingPhase = math.min(1, phase * 5)
-      local fadePhase = 1 - math.min(1, phase * 1)
+      local swingPhase = math.min(1, phase * (sword.holdTime / sword.swingTime))
+      local fadePhase = (1 - math.min(1, phase * 1)) ^ 2
       
       -- Determine the angles between which to draw the
       -- sword slash at the current point in the sword
@@ -181,27 +238,34 @@ PlayerDrawable = function(avatar)
   end
 end
 
+spawnEnemy = function(ecs) 
+  local width, height = love.window.getMode()
+  
+  ecs:create {
+    radius      = 8,
+    
+    drawable    = CircleDrawable(),
+    color       = {0x88, 0x99, 0x22},
+    
+    pos         = Vector(math.random() * width, math.random() * height),
+    controller  = ChaseController(),
+    enemy       = Enemy()
+  }
+end
+
 function love.load(args)
   entities = ECS.new()
   
-  local width, height = love.window.getMode()
   
   for i = 1, 20 do
-    entities:create {
-      radius      = 8,
-      
-      drawable    = CircleDrawable(8),
-      color       = {0x88, 0x99, 0x22},
-    
-      pos         = Vector(math.random() * width, math.random() * height),
-      controller  = ChaseController()
-    }
+    spawnEnemy(entities)
   end
   
   entities:create {
+    radius      = 16,
     isPlayer    = true,
     
-    drawable    = PlayerDrawable(CircleDrawable(16)),
+    drawable    = PlayerDrawable(CircleDrawable()),
     pos         = Vector(200, 200),
     color       = {255, 255, 255},
     controller  = PlayerController()
